7d2d3beb733446c5132799472c6d39b74a2ded60
shivam singh <shivamsingh01072003@gmail.com>
2025-09-24 10:34:42 +0530

    select added

--- src/component/MuiSelect.jsx
+++ src/component/MuiSelect.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box , TextField , MenuItemu, MenuItem } from '@mui/material'
 
 const MuiSelect = () => {
+
+    const [country , setCountry] = useState('')
+
+    const handleChange = () => {
+
+    }
+
   return (
-    <div>
-      MuiSelect
-    </div>
+  <Box width={'250px'}>
+    <TextField label='select country' select value={country} onChange={handleChange}>
+        <MenuItem value='IN'>India</MenuItem>
+        <MenuItem value='US'>USA</MenuItem>
+        <MenuItem value='AUS'>Australia</MenuItem>
+    </TextField>
+
+  </Box>
   )
 }
 
